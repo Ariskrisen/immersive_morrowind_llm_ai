583177742fa9c5d8aee06c6681178eafe2e743e1
Ariskrisen <73885216+Ariskrisen@users.noreply.github.com>
2025-06-14 19:05:18 +0700

    Add files via upload
    add voice_identifier: Optional[str] = None

--- src/server/tts/backend/abstract.py
+++ src/server/tts/backend/abstract.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+from typing import Optional
+
 from pydantic import BaseModel
 
 from tts.voice import Voice
@@ -9,6 +11,7 @@ class TtsBackendRequest(BaseModel):
     text: str
     voice: Voice
     file_path: str
+    voice_identifier: Optional[str] = None
 
 
 class TtsBackendResponse(BaseModel):
